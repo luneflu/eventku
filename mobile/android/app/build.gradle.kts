c30f8828e08676483220185cc860485bf985a2c2
mamangg277-ctrl <mamangg277@gmail.com>
2026-07-18 10:57:38 +0700

    test: verify build pipeline functionality and artifact generation

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +8,15 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+//load file Properties
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.new.EventKu"
+    namespace = "com.WOK.EventKu"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.new.EventKu"
+        applicationId = "com.WOK.EventKu"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,13 +40,24 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    // -modded
+    signingConfigs {
+        create("release") {
+            if(keystorePropertiesFile.exists()) {
+                storeFile       = file(keystoreProperties.getProperty("storeFile"))
+                storePassword   = keystoreProperties.getProperty("storePassword")
+                keyAlias        = keystoreProperties.getProperty("keyAlias")
+                keyPassword     = keystoreProperties.getProperty("keyPassword")
+            }
         }
     }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+    // -end modded
 }
 
 flutter {
